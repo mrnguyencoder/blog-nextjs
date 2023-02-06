5b3c070925d0bc72455964e6e012ec1aae7d5759
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-06 09:52:12 +0100

    onnect Next.js app to MongoDB

---
+++ models/Post.jsx
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  date: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
+
+
+import React from 'react';
+import Post from '../models/Post';
+
+const Posts = ({ posts }) => {
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post._id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export async function getServerSideProps() {
+  const posts = await Post.find().sort({ date: -1 });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
+
